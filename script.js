c2793018480e79f2187fddd133b4147565cbb51b
Manjot <078manjotsingh@gmail.com>
2023-01-20 01:45:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImage1= "images/dice"+randomNumber1+".png";
+var imageSelector1=document.querySelector(".img1").setAttribute("src",randomImage1);
+
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2= "images/dice"+randomNumber2+".png";
+var imageSelector2=document.querySelector(".img2").setAttribute("src",randomImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 2 wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
